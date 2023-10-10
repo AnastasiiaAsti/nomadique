ffcb1fc75d6c1e143f37b7fbc35044e10cd229ae
Asti <astistateofmind@gmail.com>
2023-10-09 17:35:37 -0700

    adds post models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post, Paragraph, Link
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Paragraph)
+admin.site.register(Link)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.core.files import File
+from urllib import request
+from django.contrib.postgres.fields import ArrayField
+import os
 
-# Create your models here.
+
+class Link(models.Model):
+    url = models.URLField()
+    description = models.TextField(max_length=2000)
+    image_file = models.ImageField(upload_to="images")
+    image_url = models.URLField()
+
+
+def get_remote_image(self):
+    if self.image_url and not self.image_file:
+        result = request.urlretrieve(self.image_url)
+        self.image_file.save(
+            os.path.basename(self.image_url), File(open(result[0], "rb"))
+        )
+        self.save()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    images = ArrayField(models.ImageField(upload_to="images/"))
+    link = models.ManyToManyField(Link)
+    summary = models.TextField(max_length=4000)
+    hashtags = ArrayField(models.CharField(max_length=200), blank=True)
+
+
+class Paragraph(models.Model):
+    title = models.CharField(max_length=300)
+    text = models.TextField(max_length=4000)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
